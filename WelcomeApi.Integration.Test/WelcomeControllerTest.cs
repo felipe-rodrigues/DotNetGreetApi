07881eebeb0813a6684ade107f2b2bb634124b25
Felipe Rodrigues da Silva <felipe_rodrigues-silva@outlook.com>
2020-06-22 15:25:58 -0300

    Inicial

---
+++ WelcomeApi.Integration.Test/WelcomeControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using DotNetIntegrationTestCI;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WelcomeApi.Integration.Test
+{
+    public class WelcomeControllerTest
+    {
+        public readonly HttpClient HttpClient;
+        public WebApplicationFactory<Startup> appFactory;
+
+        public WelcomeControllerTest()
+        {
+            appFactory = new WebApplicationFactory<Startup>();
+
+            HttpClient = appFactory.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task DefaultRoute_ShouldReturnHelloWorldString()
+        {
+            var req = await HttpClient.GetAsync("/api/welcome");
+
+            Assert.True(req.IsSuccessStatusCode);
+            var response = await req.Content.ReadAsStringAsync();
+
+            Assert.Equal("Hello World", response);
+        }
+    }
+}
---
+++ WelcomeApi/Controllers/WelcomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetIntegrationTestCI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public string SayHello()
+        {
+            return "Hello World";
+        }
+    }
+}
